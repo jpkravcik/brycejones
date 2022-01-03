72a77681946f1ad94996298ba0dd30993d92b51a
John Kravcik <jpkravcik@gmail.com>
2022-01-02 22:12:31 -0600

    removed assets from .gitignore

---
+++ src/assets/index.js
@@ -0,0 +1,29 @@
+import image1 from './portfolio/image1.png';
+import image2 from './portfolio/image2.png';
+import image3 from './portfolio/image3.png';
+import image4 from './portfolio/image4.png';
+import image5 from './portfolio/image5.png';
+import image6 from './portfolio/image6.png';
+import image7 from './portfolio/image7.png';
+import image8 from './portfolio/image8.png';
+import image9 from './portfolio/image9.png';
+import image10 from './portfolio/image10.png';
+import image11 from './portfolio/image11.png';
+import logo from './BryxePhotoTransparent.png';
+
+export {
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    logo
+}
+
+
